fc6445b7006deb9261a80743969a8c3c6bda9237
Alex <66533229+Alex-ardashev@users.noreply.github.com>
2023-08-09 13:16:57 +0400

    Update fact_ads_moderation_funnel_final.sql

--- fact_ads_moderation_funnel_final.sql
+++ fact_ads_moderation_funnel_final.sql
@@ -10,107 +10,21 @@ CREATE TABLE IF NOT EXISTS hive.odyn_css.fact_ads_moderation_funnel_final
   median_timediff       integer,
   dt                    date
 )
-
 WITH (
     format = 'PARQUET',
     partitioned_by = ARRAY ['DT']
 );
-
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 DELETE
 FROM   hive.odyn_css.fact_ads_moderation_funnel_final
 WHERE  dt = Date('${vars:date}') - interval '1' day;
-
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 INSERT INTO hive.odyn_css.fact_ads_moderation_funnel_final
-
-
-WITH countries_category AS
-            (
-                      SELECT    id                      AS ad_id,
-                                livesync_dbname         AS site_code,
-                                category_id,
-                                private_business
-                      FROM      hive.livesync.cee_olxkz_ads x
-                      WHERE date(created_at_first) between date('${vars:date}') - interval '1' day and date('${vars:date}')
-
-                      UNION ALL
-
-                      SELECT    id                      AS ad_id,
-                                livesync_dbname         AS site_code,
-                                category_id,
-                                private_business
-                      FROM      hive.livesync.cee_olxbg_ads x
-                      WHERE date(created_at_first) between date('${vars:date}') - interval '1' day and date('${vars:date}')
-
-                      UNION ALL
-
-                      SELECT    id                      AS ad_id,
-                                livesync_dbname         AS site_code,
-                                category_id,
-                                private_business
-                      FROM      hive.livesync.cee_olxro_ads x
-                      WHERE date(created_at_first) between date('${vars:date}') - interval '1' day and date('${vars:date}')
-
-                      UNION ALL
-
-                      SELECT    id                      AS ad_id,
-                                livesync_dbname         AS site_code,
-                                category_id,
-                                private_business
-                      FROM      hive.livesync.cee_olxuz_ads x
-                      WHERE date(created_at_first) between date('${vars:date}') - interval '1' day and date('${vars:date}')
-
-                      UNION ALL
-
-                      SELECT    id                      AS ad_id,
-                                livesync_dbname         AS site_code,
-                                category_id,
-                                private_business
-                      FROM      hive.livesync.cee_olxpl_ads x
-                      WHERE date(created_at_first) between date('${vars:date}') - interval '1' day and date('${vars:date}')
-
-                      UNION ALL
-
-                      SELECT    id                      AS ad_id,
-                                livesync_dbname         AS site_code,
-                                category_id,
-                                private_business
-                      FROM      hive.livesync.cee_olxua_ads x
-                      WHERE date(created_at_first) between date('${vars:date}') - interval '1' day and date('${vars:date}')
-
-                      UNION ALL
-
-                      SELECT    id                      AS ad_id,
-                                livesync_dbname         AS site_code,
-                                category_id,
-                                private_business
-                      FROM      hive.livesync.cee_olxpt_ads x
-                      WHERE date(created_at_first) between date('${vars:date}') - interval '1' day and date('${vars:date}')
-
-
-),
-
-category as (
-                      SELECT cast(ad_id as varchar) ad_id,
-                             site_code,
-                             category_id,
-                             private_business, category_l1_name_en AS category_l1
-                      FROM countries_category x
-                      LEFT JOIN hive.olxgroup_reservoir_ares.reservoirs_olxgroup_reservoir_eu_bi_eu_bi_dim_categories y
-                      ON     cast(x.category_id AS varchar) = y.category_nk
-
-                          and x.site_code = 'olx'||lower(country_nk)
-                          and region_sk = 'olx|eu'
-),
-
-total_verdicts AS ( 
+WITH total_verdicts AS ( 
        
 WITH verdicts_ade AS
 (
@@ -128,23 +42,50 @@ WITH verdicts_ade AS
        AND    params_content_meta_sender = 'ads-decision-engine'
        AND    params_content_event_source <> 'hermes'
 ), 
-
 verdicts_moderators AS
 (
-       SELECT cast(params_content_event_adid AS varchar)                                ad_id,
+       with verdicts as (
+SELECT cast(params_content_event_adid AS varchar)                                ad_id,
               params_content_event_sitecode                                             site_code,
               params_content_meta_type                                                  decision_manual,
               params_content_event_moderationcategory                                   moderation_category,
-              cast(from_iso8601_timestamp(params_content_event_createdat) AS timestamp) verdict_moderator_dt
+              cast(from_iso8601_timestamp(params_content_event_createdat) AS timestamp) verdict_moderator_dt,
+              params_content_event_extra_moderatoremail                                  mod_email
        FROM   hive.hydra.global_css_verdicts
        WHERE  cast(concat(year,'-',month,'-',day) AS date) BETWEEN date('${vars:date}') - interval '1' day AND    date('${vars:date}')
        AND    params_content_meta_sender = 'ad-moderation-queue'
-       AND    params_content_event_moderationcategory IN ('default', 'duplicate_ads') -- potentially add post-moderation here
+       AND    params_content_event_moderationcategory IN ('default', 'duplicate_ads')
+       --and params_content_event_adid = '826690639'
+),
+    decisions as (
+SELECT
+    distinct
+    cast (params_content_event_adid AS varchar)                                  ad_id,
+    params_content_event_type                                                    manual_action,
+     params_content_event_sitecode                                               site_code,
+    params_content_event_moderatordetails_email                                  mod_email
+FROM hive.hydra.global_css_decisions
+WHERE cast (concat(year, '-', month, '-', day) AS date) BETWEEN date ('${vars:date}') - interval '1' day AND date ('${vars:date}')
+  AND params_content_meta_sender = 'ad-moderation-queue'
+  AND params_content_meta_type = 'ad.decision.moderator'
+  and params_content_event_moderationcategory in ('default', 'duplicate_ads')
+  and params_content_event_type = 'edit'
+  --and params_content_event_adid = '826690639'
+    )
+    SELECT x.ad_id,
+           x.site_code,
+           CASE
+               WHEN x.decision_manual = 'ad.verdict.accept' and y.manual_action = 'edit' THEN 'ad.verdict.accept+edit'
+                   else x.decision_manual
+                       end as decision_manual,
+           x.moderation_category,
+           x.verdict_moderator_dt
+    from verdicts x left join decisions y
+on x.ad_id=y.ad_id and x.site_code=y.site_code and x.mod_email=y.mod_email
 ), 
-
 final_default_queue AS
 (
-          SELECT    distinct 
+          SELECT    distinct
                     x.*,
                     decision_manual,
                     y.verdict_moderator_dt,
@@ -156,13 +97,33 @@ rank() OVER (partition BY x.ad_id,x.site_code, verdict_moderator_dt ORDER BY aut
           ON        x.ad_id=y.ad_id
           AND       x.site_code=y.site_code
           AND       auto_moderation_dt < verdict_moderator_dt
-          WHERE x.source_of_moderation <> 'umbrella' 
+          WHERE x.source_of_moderation <> 'umbrella'
+          and x.source_of_moderation <> 'ads-decision-engine'
           AND y.moderation_category = 'default'
 ),
-
+   final_auto AS
+(
+          SELECT    distinct
+                    x.*,
+                    decision_manual,
+                    y.verdict_moderator_dt,
+                    date_diff('minute', auto_moderation_dt,verdict_moderator_dt) datediff,
+rank() OVER (partition BY x.ad_id,x.site_code, verdict_moderator_dt ORDER BY auto_moderation_dt desc) rank,
+                    cast(concat(x.year,'-',x.month,'-',x.day) AS date) dt
+          FROM      verdicts_ade x
+          LEFT JOIN verdicts_moderators y
+          ON x.ad_id = y.ad_id
+          AND       x.site_code=y.site_code
+          AND       auto_moderation_dt < verdict_moderator_dt
+          WHERE
+          x.source_of_moderation <> 'umbrella'
+          AND y.moderation_category IS NULL
+          AND y.verdict_moderator_dt IS NULL
+)
+,
 final_duplicate_queue AS
 (
-          SELECT    distinct 
+          SELECT    distinct
                     x.*,
                     decision_manual,
                     y.verdict_moderator_dt,
@@ -174,21 +135,23 @@ rank() OVER (partition BY x.ad_id,x.site_code, verdict_moderator_dt ORDER BY aut
           ON
           (
                     x.ad_id = y.ad_id
-          OR        x.parent_adid = y.ad_id 
+          OR        x.parent_adid = y.ad_id
           )
           AND       x.site_code=y.site_code
           AND       auto_moderation_dt < verdict_moderator_dt
-          WHERE x.source_of_moderation = 'umbrella' 
+          WHERE x.source_of_moderation = 'umbrella'
           AND y.moderation_category = 'duplicate_ads'
 ),
-
 final as (
 select * from final_default_queue
+where rank = 1
+UNION ALL
+select * from final_auto
+where rank = 1
 UNION ALL
 select * from final_duplicate_queue
+where rank = 1
 )
-          
-
 SELECT ad_id,
        site_code,
        source_of_moderation,
@@ -206,29 +169,39 @@ SELECT ad_id,
        datediff,
        dt
 FROM   final
-WHERE  rank = 1
-), 
-
-final_table AS
+),
+categories as 
 (
-          SELECT    x.ad_id,
+       select *,
+       rank() OVER (partition BY ad_id,site_code, category_id ORDER BY dt desc) rank
+       from hive.odyn_css.fact_ads_categories_and_type
+       where dt between Date('${vars:date}') - interval '60' day AND  Date('${vars:date}') -- potentially could be changed
+),
+categories_final as
+(
+       select * from categories
+       where rank = 1
+),
+final_table AS 
+(
+          SELECT    DISTINCT
+                    x.ad_id,
                     x.site_code,
                     category_l1,
-                    y.private_business,
+                    private_business,
                     source_of_moderation,
                     decision_auto,
                     decision_manual,
                     auto_moderation_dt,
                     verdict_moderator_dt, --has all the timings
                     datediff,
-                    dt
+                    x.dt
           FROM      total_verdicts x
-          LEFT JOIN category y
+          LEFT JOIN categories_final y
           ON        x.ad_id = y.ad_id
-          AND       x.site_code = y.site_code
+          AND       x.site_code = y.site_code       
 )
           
-
 SELECT   site_code,
          category_l1,
          Cast(private_business AS VARCHAR),
@@ -237,7 +210,6 @@ SELECT   site_code,
          decision_manual,
          Count(ad_id) AS total_ads,
          datediff,
-         --approx_percentile(datediff,0.5) as median_timediff,
          dt
 FROM     final_table
 WHERE    dt = Date('${vars:date}') - interval '1' day
